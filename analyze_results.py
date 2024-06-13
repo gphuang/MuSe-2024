3aec61e6f1e8c44594d0c82a329f2d1f15de82d5
GP <gpuhuang@gmail.com>
2024-06-13 16:15:01 +0300

    analyze results csv

--- analyze_results.py
+++ analyze_results.py
@@ -1,30 +1,84 @@
-import os, sys
+import os, sys, re
 import pandas as pd
 import numpy as np 
 import ast
+import pathlib
+from pathlib import Path
+from matplotlib import pyplot as plt
 
-model_types=('cnn', 'rnn', 'crnn', 'cnn-attn', 'crnn-attn')
-labels=('aggressive' , 'arrogant' , 'dominant' , 'enthusiastic' , 'friendly' , 'leader_like' , 'likeable' , 'assertiv' , 'confident' , 'independent' , 'risk' , 'sincere' , 'collaborative' , 'kind' , 'warm' , 'good_natured') 
-features= ('faus', 'facenet512', 'vit-fer', 'w2v-msp', 'egemaps --normalize', 'ds')  # ('hubert-superb', 'hubert-er') # 
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True}) # avoid xlabels to be cut off
 
-csv='results/csvs/perception.csv'
-df = pd.read_csv(csv, index_col=0)
-print(df.shape)
+model_types=('rnn', 'cnn', 'crnn', 'cnn-attn', 'crnn-attn')
+features=('egemaps', 'ds', 'w2v-msp', 'faus', 'vit-fer', 'facenet512', 'hubert-superb', 'hubert-er')  
+labels=('aggressive', 'arrogant', 'assertiv', 'confident', 'dominant', 'independent', 'risk', 'leader_like', 
+        'collaborative', 'enthusiastic', 'friendly', 'good_natured', 'kind', 'likeable', 'sincere',  'warm') 
+
+csv_in='results/csvs/perception.csv'
+csv_path='results/csvs/perception_analysis.csv'
+
+# output from script, missing performance for each of 5 seeds
+df0 = pd.read_csv(csv_in, index_col=0)
+
+# get Pearson from log file, output new csv 
 if 0:
-    _out = df.groupby(['feature', 'model_type'])['best_val_Pearson'].agg([np.mean, np.std])
-    print(_out)
+    os.remove(csv_path)
+    for _model in model_types:
+        for _feat in features:
+            for _label in labels:
+                _df = df0[(df0.feature==_feat) & (df0.label_dim==_label) & (df0.model_type==_model)]
+                _dict = ast.literal_eval(_df['paths'].values[0])
+                model_name = os.path.basename(_dict['model'])
+                log_fname = os.path.join(_dict['log'], model_name + '.txt')
+                assert os.path.exists(log_fname)
+                # Open and read the file.
+                with open(log_fname, 'rt', encoding='utf-8') as f:
+                    text = f.read()
+                # "ID/Seed 101 | Best [Val Pearson]: 0.1720 |  Loss: ..."
+                # "ID/Seed 101 | Best [Val Pearson]:-0.0965 | Loss:..."
+                # Extract all the data tuples with a findall() each tuple is: (seed, Best Val Pearson)
+                # (\d+\.\d+)
+                # ^-?
+                try:
+                    tuples = re.findall(r'ID\/Seed\s(\d+)\s\|\sBest\s\[Val\sPearson\]\:\s*(-?\d+\.\d+)\s\|\s', text)
+                    dct = {'val_Pearson': [float(v) for (k,v) in tuples]}
+                    dct.update({'seed': [k for (k,v) in tuples]})
+                except:
+                    sys.exit(log_fname)
+                dct.update({'model_type':_model})
+                dct.update({'feature':_feat})
+                dct.update({'label':_label})
+                df = pd.DataFrame(dct)
 
-for _model in model_types:
-    for _feat in features:
-        for _label in labels:
-            _df = df[(df.feature==_feat) & (df.label_dim==_label) & (df.model_type==_model)]
-            _dict = ast.literal_eval(_df['paths'].values[0])
-            model_name = os.path.basename(_dict['model'])
-            log_fname = os.path.join(_dict['log'], model_name + '.txt')
-            print(model_name, log_fname)
-            assert os.path.exists(log_fname)
-            # "ID/Seed 101 | Best [Val Pearson]: 0.1720 |  Loss: ..."
-            # seed: 
-            # val_pearson:
-            sys.exit(0)
+                # make sure the directory exists
+                csv_dir = pathlib.Path(csv_path).parent.resolve()
+                os.makedirs(csv_dir, exist_ok=True)
 
+                # write back
+                if os.path.exists(csv_path):
+                    old_df = pd.read_csv(csv_path)
+                    df = pd.concat([old_df, df])
+                df.to_csv(csv_path, index=False)
+                
+df = pd.read_csv(csv_path)
+
+# group by labels
+df_out = df.groupby(['model_type', 'feature', 'label'])['val_Pearson'].agg([np.max, np.mean, np.std])
+df_out.to_csv('results/csvs/perception_analysis_table2_labels.csv')
+print(df_out)
+
+# group by seeds
+df_out = df.groupby(['model_type', 'feature'])['val_Pearson'].agg([np.max, np.mean, np.std])
+df_out.to_csv('results/csvs/perception_analysis_table3_seeds.csv')
+print(df_out)
+
+# png
+plot = df_out.plot(kind ='bar', title="Group by seeds")
+
+
+ax = df_out.plot.bar()
+ax.set_title("Group by seeds",color='black')
+ax.legend(bbox_to_anchor=(1.0, 1.0))
+ax.plot()
+plt.tight_layout()
+plt.savefig('results/pngs/perception_analysis_table3_seeds.png')
